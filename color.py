96ff833915506d0354662ed79b21b17a27ac071b
Tux <77721526+tuxys@users.noreply.github.com>
2021-01-20 01:59:25 -0700

    Update color.py

--- color.py
+++ color.py
@@ -1,7 +1,10 @@
-#!/bin/python -B
+#!/bin/python
 #
-# Colors In Terminal
-# {2021-01-20}
+# Colors In Terminal v0.1
+# By Tux {2021-01-20}
+#
+# Requires: Python 3.9+ & sty module for colors.
+# Thank you to Alex Beals for Colorize project.
 #
 
 import sys, re
@@ -11,31 +14,26 @@ from urllib.request import urlopen
 
 site = 'https://alexbeals.com/projects/colorize/search.php?q='
 
-try:
+cmd = sys.argv
+if len(cmd) <= 1:
+	print('usage: <word>')
+	sys.exit()
+elif cmd[1]:
+	find = cmd[1]
+else: sys.exit()
 
-	cmd = sys.argv
-	if len(cmd) <= 1:
-		print('usage: <word>')
-		sys.exit()
-	elif cmd[1]:
-		find = cmd[1]
-	else: sys.exit()
+data = urlopen(f'{site}{find}')
+html = data.read().decode('utf-8')
 
-	data = urlopen(f'{site}{find}')
-	html = data.read().decode('utf-8')
+chex = re.search('hex\'\);">#(.*?)</span>', html).group(1)
+crgb = re.search('rgb\'\);">rgb\((.*?)\)</span>', html).group(1)
 
-	chex = re.search('hex\'\);">#(.*?)</span>', html).group(1)
-	crgb = re.search('rgb\'\);">rgb\((.*?)\)</span>', html).group(1)
+slen = ' '.ljust(16)
+srgb = crgb.split(',')
 
-	slen = ' '.ljust(16)
-	srgb = crgb.split(',')
+bg.cl = Style(RgbBg(srgb[0], srgb[1], srgb[2]))
 
-	bg.cl = Style(RgbBg(srgb[0], srgb[1], srgb[2]))
+print(f'\n HEX: {chex}\n RGB: {crgb}')
+print(f' {bg.cl}{slen}{bg.rs}\n')
 
-	print(f'\n HEX: {chex}\n RGB: {crgb}')
-	print(f' {bg.cl}{slen}{bg.rs}\n')
-
-except KeyboardInterrupt:
-	print('Exited'); sys.exit();
-
-#EOF: ./color.py
+#EOF: color.py
